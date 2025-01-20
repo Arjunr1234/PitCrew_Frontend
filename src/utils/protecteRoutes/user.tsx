d9ba3ad0d0e8b04f17b0f46bcd0d80228359b7c7
Arjunr1234 <arjunreji1234@gmail.com>
2025-01-20 19:46:22 +0530

    change protetcted route to login

--- src/utils/protecteRoutes/user.tsx
+++ src/utils/protecteRoutes/user.tsx
@@ -8,7 +8,7 @@ const ProtectedRoute = () => {
 
     
     if (!userInfo) {
-        return <Navigate to="/" replace />;
+        return <Navigate to="/login" replace />;
     }
 
     return <Outlet />; 
